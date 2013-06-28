74465fc0ec1afd587c1b93a69fccae0f684c41d3
chrisamico <eyeseast@gmail.com>
2013-06-28 18:22:37 -0400

    Videos are linkable

--- README.md
+++ README.md
@@ -8,4 +8,17 @@ How it works
 
 There are thousands of lectures on YouTube. [Here's Yale](http://www.youtube.com/user/YaleCourses). Let's pull those in and serve a random one everytime you load. Use it as background noise or to go to sleep at night.
 
-Pull in an RSS feed of videos. Store the oEmbed data for each video URL in a Redis set.
+Pull in an RSS feed of videos. Store the user:video_id pairs for each video URL in a Redis set. Pull in video details from the YouTube (v2) API on display (so it's always up to date).
+
+
+TODO:
+-----
+
+ - videos are linkable
+ - make colors less Microsofty
+ - improve mobile layout; video should be above description.
+ - rewrite loader to use spreadsheet and accept arbitrary feeds
+ - buy a domain
+ - analytics
+ - ads
+ - channels?
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ The main app. This runs everything.
 import os
 import urlparse
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flaskext.markdown import Markdown
 
 from connection import redis, key, get_video_id, get_video
@@ -22,8 +22,21 @@ def index():
     """
     Show a random video, pulled from our redis set.
     """
-    user, vid = redis.srandmember(key()).split(':')
-    video = get_video(user, vid)
+    user, id = redis.srandmember(key()).split(':')
+    return redirect(url_for('video', user=user, id=id))
+
+#@app.route('/<user>')
+def channel(user):
+    """
+    Show videos from one channel.
+    """
+
+@app.route('/<user>/<id>')
+def video(user, id):
+    """
+    Show a video.
+    """
+    video = get_video(user, id)
     video.id = get_video_id(video.link)
 
     return render_template('video.html', video=video)
